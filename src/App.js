9f88500e3a2d7ef684ce2f0202cf52ea3e2e83b0
ValeriaSanabria <valeriasanabria@tdf.edu.ar>
2023-07-06 14:39:30 -0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-import {Modal} from '././componentes/modal'
 
 function App() {
 
   return (
-    <main>
       <div>
         <div className="container">
           <button>Modal 1</button>
         </div>
+
         
-        <Modal />
       </div>
-    </main>
     );
 
 }
